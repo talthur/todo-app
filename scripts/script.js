158fc43067f33910eca8006750dae06c5031c124
Arthur Gomes <talthur.ga@gmail.com>
2020-12-23 19:59:21 -0300

    Ajustes para responsividade do APP no CSS. Organização de código e inserção de comentários.

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,49 @@
-var addButton = document.getElementById("add-button");
-addButton.addEventListener("click", addToList);
-
+// DECLARAÇÃO DE VARIAVEIS PARA SELECIONAR OS TRECHOS DO HTML
 var toDoEntryBox  = document.getElementById("todo-list-form");
 var toDoList = document.getElementById("todo-list");
 
-toDoList.onkeyup = function(e){
+
+
+//BLOCO PARA QUE A TECLA ENTER FUNCIONE PARA ENVIAR OS DADOS ALÉM DO BOTÃO SUBMIT
+toDoEntryBox.onkeydown = function(e){
     if(e.keyCode == 13){
         addToList();
+        return false;
     }
 }
 
+
+//ADICIONA O EVENT LISTENER AO BOTÃO SUBMIT E EXECUTA FUNÇÃO DE INSERIR NOMES
+var addButton = document.getElementById("add-button");
+addButton.addEventListener("click", addToList);
+
+
+//ATRIBUI O TEXTO DIGITADO NO FORM A UMA VARIAVEL E A PASSA COMO PARAMETRO PARA A FUNÇÃO NEWTODOITEM, PASSA O PARAMETRO 'COMPLETED' COMO FALSE
 function addToList(){
     var itemText = toDoEntryBox.value;
     newToDoItem(itemText, false)
 };
 
+
+//CRIA UM ELEMENTO DO TIPO LISTA(LI) E INSERE O CONTEÚDO DA VARIAVEL DA FUNÇÃO ANTERIOR DENTRO DO ELEMENTO. ADICION
 function newToDoItem(itemText, completed){
 
-    var toDoItem = document.createElement("li");
-    var toDoText = document.createTextNode(itemText);
-    toDoItem.appendChild(toDoText);
+    var toDoItem = document.createElement("li"); //cria elemento LI
+    var toDoText = document.createTextNode(itemText);//cria variável com o conteúdo do texto digitado
+    toDoItem.appendChild(toDoText);//faz um append como filho do texto digitado ao elemento LI
 
     if (completed){
-        toDoItem.classList.add("completed");
+        toDoItem.classList.add("completed");//caso o parametro 'completed' for verdadeiro adiciona a classe 'completed' ao elemento LI
     }
 
-    toDoList.appendChild(toDoItem);
-    toDoItem.addEventListener("dblclick", toggleToDoItemState);
-
-
+    toDoItem.addEventListener("dblclick", toggleToDoItemState); //adiciona o listener para quando tocar duas vezes iniciar a função de trocar classe
+    toDoList.appendChild(toDoItem);//adiciona o elemento LI agora com conteúdo ao elemento pai OL
+    
 };
 
+
+
+//TROCA A CLASSE DA TAG. SE ESTIVER COM A CLASSE 'COMPLETED' ADIONA A CLASSE, CASO CONTRÁRIO A REMOVE
 function toggleToDoItemState(){
     if (this.classList.contains("completed")){
         this.classList.remove("completed")
@@ -40,33 +53,31 @@ function toggleToDoItemState(){
 
 }
 
+
+
+//ADICIONA O EVENT LISTENER AO BOTÃO LIMPAR COMPLETADAS
 var clearCompleted = document.getElementById("clear-complete-button");
 clearCompleted.addEventListener("click", clearCompletedItems);
 
+
+//LIMPA ITENS COMPLETADOS DA LISTA
 function clearCompletedItems(){
 
-    var allCompletedItems = toDoList.getElementsByClassName("completed")
-    console.log(allCompletedItems);
+    var allCompletedItems = toDoList.getElementsByClassName("completed")//cria uma variável selecionando somente os elementos da classe "completed" presentes na OL
 
-    while (allCompletedItems.length > 0) {
+    while (allCompletedItems.length > 0) { 
 
         allCompletedItems.item(0).remove();
-
-    }
+    }// faz um laço removendo cada elemento com a classe "completed"
 };
 
 
-var saveButton = document.getElementById("save-button");
-saveButton.addEventListener("click", saveList);
 
-function saveList(){
-    alert("Botao funcionando")
-};
-
-
+//ADICIONA O EVENT LISTENER AO BOTÃO ESVAZIAR LISTA
 var emptyButton = document.getElementById("empty-button");
 emptyButton.addEventListener("click", emptyList);
 
+//LIMPA TODOS ITENS DA LISTA
 function emptyList(){
 
     var allItemsToRemove = toDoList.getElementsByTagName("li");
@@ -75,11 +86,38 @@ function emptyList(){
 
         allItemsToRemove.item(0).remove();
 
+    }//cria uma variavel com todos elementos "li" dentro da OL e os remove um por um atráves de um laço
+
+
+};
+
+
+//ADICIONA O EVENT LISTENER AO BOTÃO SALVAR LISTA
+var saveButton = document.getElementById("save-button");
+saveButton.addEventListener("click", saveList);
+
+function saveList(){
+
+    var toDos = [];
+
+    for (var i = 0; i < toDoList.children.length; i++){
+        toDo = toDoList.children.item(i)
+
+        var toDoInfo = {
+            "task":toDo.innerText,
+            "completed":toDo.classList.contains("completed")
+        }
+
+        toDos.push(toDoInfo);
+
     }
 
+    localStorage.setItem("toDos", JSON.stringify(toDos));
 
-
-    
-    // alert("botao funcionando");
+    alert("Botao funcionando")
 };
+
+
+
+
 
